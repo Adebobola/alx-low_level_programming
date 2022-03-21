71d1fae56deb4007b487adfdce1c38a626383537
Adebobola <64739309+Adebobola@users.noreply.github.com>
2022-03-21 22:48:12 +0100

    commit

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,11 +1,20 @@
 #include "main.h"
+
 /**
- * reset_to_98 - takes a pointer and updates the value it point to
- * @n: pointer
- * Return: void
+ * swap_int - function that swaps the values of two integers.
+ *
+ * @a: number 1.
+ *
+ *@b: number 2.
+ *
+ * Return: 0.
  */
-void swap_int(int *a, int *b) {
-  int x = *a;
-	*a = *b;
-	*b = x;
+
+void swap_int(int *a, int *b)
+{
+int c;
+
+c = *a;
+*a = *b;
+*b = c;
 }
