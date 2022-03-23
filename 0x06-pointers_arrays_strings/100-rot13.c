42ce4157bce6e00a1892063107a707d8ae8e0c35
Adebobola <64739309+Adebobola@users.noreply.github.com>
2022-03-23 22:21:23 +0100

    commit

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,28 +1,29 @@
 #include "main.h"
+
 /**
- * rot13 - encodes strings using rot13.
- * @s: pointer to string.
+ * rot13 - encode string  you rot 13 so move 13 spaces.
+ * @str: string to be encoded
  *
- * Return: pointer to encoded string.
+ * Return: encoded string.
  */
+char *rot13(char *str)
+{
+	int i, j;
+	char arr1[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char arr2[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
-char *rot13(char *s)
-{
-int stringCount, rotation;
 
-char r1 [] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-char r2[] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'};
-for (stringCount = 0; s[stringCount] != '\0'; stringCount++)
-{
-for (rotation = 0; rotation < 53; rotation++)
-{
-if (r1[rotation] == s[stringCount])
-{
-s[stringCount] = r2[rotation];
-break;
-}
-}
-}
-return (s);
+	for (i = 0; str[i] != '\0'; ++i)
+	{
+		for (j = 0; j < 52; ++j)
+		{
+			if (str[i] == arr1[j])
+			{
+				str[i] = arr2[j];
+				break;
+			}
+		}
+	}
+	return (str);
 }
 
