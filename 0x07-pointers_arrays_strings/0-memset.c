fdc96b3e752e9736e5466da4c4251154a86afd0c
Adebobola <64739309+Adebobola@users.noreply.github.com>
2022-03-28 21:59:25 +0100

    commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,17 +5,18 @@
  * @s: memory area to be filled
  * @b: char to copy
  * @n: number of times to copy b
- *
  * Return: pointer to the memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; i < n; i++)
+	i = 0;
+
+	while (i < n)
 	{
 		s[i] = b;
+		i++;
 	}
-
 	return (s);
 }
